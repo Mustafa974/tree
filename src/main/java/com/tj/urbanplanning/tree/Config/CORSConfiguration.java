9df202f0c1d6c9026e6fe9a2aaccccad9f07192b
Mustafa <920998500@qq.com>
2019-07-02 21:43:07 +0800

    final final final

--- src/main/java/com/tj/urbanplanning/tree/Config/CORSConfiguration.java
+++ src/main/java/com/tj/urbanplanning/tree/Config/CORSConfiguration.java
@@ -3,11 +3,13 @@ package com.tj.urbanplanning.tree.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 //CORS跨域访问配置
 @Configuration
+@EnableWebMvc
 public class CORSConfiguration {
 
     @Bean
@@ -16,8 +18,11 @@ public class CORSConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:63342")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin",
+                                "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
                         .allowCredentials(true).maxAge(3600);
             }
         };
